dd9b3ac21f61a87cfc80933f0a8cc6a29bc79533
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2023-05-30 23:35:37 +0300

    simit çay compare

--- script.js
+++ script.js
@@ -8,9 +8,20 @@ function calculate() {
     // Hesaplamaları yap
     var dailyLostValue = (tlAmount * dailyInflationRate) / 100;
     var dailyLostDollarValue = dailyLostValue / dollarExchangeRate;
+
+    var lastYearBagelPrice = 3.5;
+    var currentBagelPrice = 7;
+
+    var lastYearTeaPrice = 4;
+    var currentTeaPrice = 8;
   
     // Sonuçları göster
     var result = document.getElementById("result");
-    result.innerHTML = `Bugün ${dailyLostValue.toFixed(2)} TL fakirleştin, Ayda: ${(dailyLostValue * 30).toFixed(2)} TL, Yılda ${(dailyLostValue * 365).toFixed(2)} TL fakirleşeceksin`
+    result.innerHTML = "";
+    result.innerHTML += `<p> Bugün ${dailyLostValue.toFixed(2)} TL fakirleştin,<br/>Bu kafayla gidersen ayda ${(dailyLostValue * 30).toFixed(2)} TL, yılda ${(dailyLostValue * 365).toFixed(2)} TL fakirleşeceksin.</p>`
+
+    result.innerHTML += `<u>Geçen sene bu parayla ortalama:</u><br/>`
+    result.innerHTML +=  `<li>${Math.floor(tlAmount / lastYearBagelPrice)} simit alabiliyordun, Bu yıl ortalama ${Math.floor(tlAmount / currentBagelPrice)} simit alabiliyorsun.</li>`
+    result.innerHTML += ` <li>${Math.floor(tlAmount / lastYearTeaPrice)} bardak çay içebiliyordun, Bu yıl ortalama ${Math.floor(tlAmount / currentTeaPrice)} bardak çay içebiliyorsun.</li>`
   }
   
