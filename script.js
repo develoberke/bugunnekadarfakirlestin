7d695411e780578b9afcffb4199b541ef2a70fd9
T1murY <yasartim25@gmail.com>
2023-05-31 11:23:37 +0300

    dollar exchange rate

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function calculate() {
+    const http = new XMLHttpRequest()
+    http.open("GET", "https://api.freecurrencyapi.com/v1/latest?apikey=yTsLyX9C1EHf6qIFv8I5AOdxAaKMCACYuWglMhJw&currencies=TRY")
+    http.send()
+    http.onload = () => {
     var tlAmount = parseFloat(document.getElementById("tlAmount").value);
-
     var annualInflationRate = 72.31;
     var dailyInflationRate = annualInflationRate / 365;
-    var dollarExchangeRate = 20.40;
-  
     // Hesaplamaları yap
     var dailyLostValue = (tlAmount * dailyInflationRate) / 100;
     var dailyLostDollarValue = dailyLostValue / dollarExchangeRate;
-
+    var dollarExchangeRate = JSON.parse(http.responseText).data.TRY;
     var lastYearBagelPrice = 3.5;
     var currentBagelPrice = 7;
 
@@ -23,5 +24,7 @@ function calculate() {
     result.innerHTML += `<u>Geçen sene bu parayla ortalama:</u><br/>`
     result.innerHTML +=  `<li>${Math.floor(tlAmount / lastYearBagelPrice)} simit alabiliyordun, Bu yıl ortalama ${Math.floor(tlAmount / currentBagelPrice)} simit alabiliyorsun.</li>`
     result.innerHTML += ` <li>${Math.floor(tlAmount / lastYearTeaPrice)} bardak çay içebiliyordun, Bu yıl ortalama ${Math.floor(tlAmount / currentTeaPrice)} bardak çay içebiliyorsun.</li>`
-  }
+    result.innerHTML += `<p>Dolar kuru saatlik güncellenir: ${dollarExchangeRate} </p>`
+    result.innerHTML += `<p>Elindeki para suan ${Math.floor(tlAmount / dollarExchangeRate)} dolar ediyor</p>`
+  }}
   
