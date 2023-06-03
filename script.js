84138aa7e08c2779872bb89833bcf1d38ccfc120
BBerkeYildirim <batuhanberke.yildirim@gmail.com>
2023-06-03 16:21:32 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 async function calculate() {
     var result_loading = document.getElementById("result-loading");
+    var result = document.getElementById("result");
+    var compareList = document.getElementById("compareList")
+
+    result.style.display = "none";
+    compareList.style.display = "none";
     result_loading.style.display = "flex";
 
     var tlAmount = parseFloat(document.getElementById("tlAmount").value);
@@ -10,12 +15,14 @@ async function calculate() {
     var lastYearDollarExchangeRate = await getLastYearDollarExchangeRate(lastYearTodayDate);
     var dollarExchangeRate = await getCurrentYearDollarExchangeRate(todayDate);
     result_loading.style.display = "none";
+    compareList.style.display = "block"
+    result.style.display = "block"
 
 
     var dailyInflationRate = annualInflationRate / 365;
     var dailyLostValue = (tlAmount * dailyInflationRate) / 100;
 
-    var result = document.getElementById("result");
+   
     result.innerHTML = "";
     result.innerHTML += `<div><p class="resultText">Bugün ${dailyLostValue.toFixed(2)} TL fakirleştin</p><br/>Bu kafayla gidersen ayda ${(dailyLostValue * 30).toFixed(2)} TL, yılda ${(dailyLostValue * 365).toFixed(2)} TL fakirleşeceksin.<br/></div>`
 
@@ -24,9 +31,6 @@ async function calculate() {
 
     var dollarText = document.getElementById("dollarText")
     dollarText.innerHTML = `Geçen Yıl Ortalama: ${Math.floor(tlAmount / lastYearDollarExchangeRate)} dolar<br/>Bu Yıl Ortalama: ${Math.floor(tlAmount / dollarExchangeRate)} dolar<br/>`
-    
-    var compareList = document.getElementById("compareList")
-    compareList.style.display = "block"
 
 
     var lastYearBagelPrice = 3.5;
